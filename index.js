b31e11a47052aa85832311fe1e19e5b720594d9d
Harry Oyarvide <poyarvide87@yahoo.com>
2024-11-07 11:57:44 -0500

    Added array with objects.

--- index.js
+++ index.js
@@ -3,7 +3,64 @@ import inquirer from "inquirer";
 import fs from "fs";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: colors.brightMagenta('What is your project name?'),
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: colors.brightMagenta('Can you provide a brief description of the project?'),
+    },
+    {
+      type: 'input',
+      name: 'problem',
+      message: colors.brightMagenta('What problem does this project solve?'),
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: colors.brightMagenta('What are the steps to install and set up this project?'),
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: colors.brightMagenta('How do you use this project?'),
+    },
+    {
+      type: 'input',
+      name: 'screenshot',
+      message: colors.brightMagenta('Do you have any screenshots to include? (Provide paths or URLs)'),
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: colors.brightMagenta('Do you welcome contributions to the project? (Yes/No)'),
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: colors.brightMagenta('How can users test the project?'),
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: colors.brightMagenta('What license is this project under?'),
+    },
+    {
+      type: 'input',
+      name: 'contact',
+      message: colors.brightMagenta('Who should users contact for questions or support? (Provide an email or link)'),
+    },
+    {
+      type: 'input',
+      name: 'acknowledgments',
+      message: colors.brightMagenta('Do you want to acknowledge any libraries, tools, or people?'),
+    }
+  ];
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
